47b02f270a2e2c0db9994ac046b6199b0d1db89b
elmar.hinz <elmar.hinz@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-05-09 21:51:11 +0000

    Added ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tsc/trunk@5459 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "tsc"
+#
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TypoScript Compiler (TSC)',
+	'description' => 'Tokenizer and syntax checker,  that compiles TypoScript to a PHP array.',
+	'category' => 'plugin',
+	'author' => 'Elmar Hinz',
+	'author_email' => 'elmar.hinz@team-red.net',
+	'author_company' => 'Team Red',
+	'version' => '0.2.0',
+);
+
+?>
